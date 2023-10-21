3cd353cfcd73e5a6a4131d1ad31dc08fed9e1e1d
JapaneseZgredek <84537728+JapaneseZgredek@users.noreply.github.com>
2023-10-21 23:57:19 +0200

    Add possibility to guess a letter, see history of guesses

--- main.py
+++ main.py
@@ -12,21 +12,58 @@ def read_from_file() -> list:
     return list_of_words
 
 
+def get_indexes_of_guessed_letter(user_guess: str, word_to_guess: str) -> list:
+    indexes = []
+    for i in range(len(word_to_guess)):
+        if user_guess == word_to_guess[i]:
+            indexes.append(i)
+    return indexes
+
+
 def main():
     global LIVES
     list_of_words = read_from_file()
     word_to_guess = list_of_words[random.randint(0, len(list_of_words))]
     correct_guesses = 0
+    did_guess_letter = [False] * len(list_of_words)
+    history_user_input = []
     print(word_to_guess)
+
     while LIVES > 0 and correct_guesses != len(word_to_guess):
-        print('WORD: ' + ('-' * len(word_to_guess)))
-        user_guess = input('Guess the letter or the whole word: ')
+        print("Enter 1 to see the history of guesses: ")
+        for i in range(len(word_to_guess)):
+            if did_guess_letter[i]:
+                print(word_to_guess[i], end="")
+            else:
+                print('-', end="")
+
+        user_guess = input('\nGuess the letter or the whole word: ')
+
+        if user_guess == '1':
+            print(history_user_input)
+            continue
+
+        if user_guess in history_user_input:
+            print('You already tried tha one. try again')
+            continue
+        history_user_input.append(user_guess)
+
         if len(user_guess) > 1:
             if user_guess == word_to_guess:
                 correct_guesses = len(word_to_guess)
                 continue
             LIVES -= 1
-    
+
+        indexes_of_guessed = get_indexes_of_guessed_letter(user_guess, word_to_guess)
+        correct_guesses += len(indexes_of_guessed)
+
+        if len(indexes_of_guessed) == 0:
+            print("WRONG GUESS")
+            LIVES -= 1
+
+        for index in indexes_of_guessed:
+            did_guess_letter[index] = True
+
     if LIVES > 0:
         print('CONGRATS YOU WON 👑')
     else:
