aaf0b4e7999f9823cac857088a547190f2b8af0e
JapaneseZgredek <84537728+JapaneseZgredek@users.noreply.github.com>
2023-10-21 23:37:28 +0200

    Let user guess a word

--- main.py
+++ main.py
@@ -13,14 +13,25 @@ def read_from_file() -> list:
 
 
 def main():
+    global LIVES
     list_of_words = read_from_file()
     word_to_guess = list_of_words[random.randint(0, len(list_of_words))]
     correct_guesses = 0
+    print(word_to_guess)
     while LIVES > 0 and correct_guesses != len(word_to_guess):
         print('WORD: ' + ('-' * len(word_to_guess)))
         user_guess = input('Guess the letter or the whole word: ')
+        if len(user_guess) > 1:
+            if user_guess == word_to_guess:
+                correct_guesses = len(word_to_guess)
+                continue
+            LIVES -= 1
+    
+    if LIVES > 0:
+        print('CONGRATS YOU WON 👑')
+    else:
+        print('YOU LOST :(')
 
 
 if __name__ == '__main__':
     main()
-
